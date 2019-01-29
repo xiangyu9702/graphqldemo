39896285c8425eb6b534c2d9ae06dfd1538962bf
xiangyu9702 <xiangyuchen9702@gmail.com>
2019-01-29 09:30:51 +0800

    update

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,16 +1,8 @@
 package com.example;
 
-import com.example.web.Institute.Institute;
-import com.example.web.Institute.InstituteDatabase;
-import com.example.web.CollegeService;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
-import java.util.ArrayList;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DemoApplication {
